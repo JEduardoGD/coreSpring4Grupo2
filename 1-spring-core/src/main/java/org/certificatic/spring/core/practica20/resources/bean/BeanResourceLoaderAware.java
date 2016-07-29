76b6bb1849d2bfed9ac7f7f44f31289386a124c9
Ivan Van Halen <Ivan Van Halen>
2016-07-28 23:39:20 -0500

    clone laboratiorios

---
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica20/resources/bean/BeanResourceLoaderAware.java
@@ -0,0 +1,10 @@
+package org.certificatic.spring.core.practica20.resources.bean;
+
+import lombok.Data;
+
+@Data
+// Definir bean
+// Implementar ResourceLoaderAware
+// Injectar bean Resources
+public class BeanResourceLoaderAware {
+}
---
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica21/methodinjection/bean/SingletonBean.java
@@ -0,0 +1,20 @@
+package org.certificatic.spring.core.practica21.methodinjection.bean;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SingletonBean {
+
+	// Define la estructura para inyectar métodos en éste bean
+	private @Setter Integer iterations;
+
+	public String process(String data) {
+		log.info("SingletonBean id [{}]: sending to process data ...",
+				super.hashCode());
+
+		// Inyecta un prototype IProcessor en este bean singleton
+		return null;
+	}
+
+}
