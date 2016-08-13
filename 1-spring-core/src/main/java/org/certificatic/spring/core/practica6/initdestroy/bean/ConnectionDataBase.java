0f5d4fd7832f465584485d69d16e9f6cd160752b
Ivan Van Halen <Ivan Van Halen>
2016-08-13 10:09:04 -0500

    practica 6 y 7 a medias

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica6/initdestroy/bean/ConnectionDataBase.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica6/initdestroy/bean/ConnectionDataBase.java
@@ -1,25 +1,57 @@
 package org.certificatic.spring.core.practica6.initdestroy.bean;
 
-import lombok.Data;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Data
-public class ConnectionDataBase {
+public class ConnectionDataBase implements InitializingBean, DisposableBean {
 	private String database;
 	private String user;
 	private String password;
 
+	public void initMethod() {
+		System.out.println("executing default init-method");
+	}
+
+	public void destroyMethod() {
+		System.out.println("executing default destroy-method");
+	}
+
 	public void connect() {
-		String msg = String.format("> Connecting to: %s:%s:%s**** [%s]", database, user,
-				password.substring(0, password.length() / 4), Integer.toHexString(super.hashCode()));
+		String msg = String.format("> Connecting to: %s:%s:%s**** [%s]",
+				database, user, password.substring(0, password.length() / 4),
+				Integer.toHexString(super.hashCode()));
 
 		System.out.println(msg);
 	}
 
 	public void disconnect() {
-		String msg = String.format("> Disconnect from: %s:%s [%s]", database, user,
-				Integer.toHexString(super.hashCode()));
+		String msg = String.format("> Disconnect from: %s:%s [%s]", database,
+				user, Integer.toHexString(super.hashCode()));
 
 		System.out.println(msg);
+	}
+
+	public void showInfo() {
+		String msg = String.format("INFO> Connected to: %s:%s [%s]", database,
+				user, Integer.toHexString(super.hashCode()));
+
+		System.out.println(msg);
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Executing destroy from disposable bean interface");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println(
+				"Executing after properties set from initializing bean interface");
 	}
 
 }
