dd5af9f697d2b59069754c0c304082f5820724d6
Ivan Van Halen <Ivan Van Halen>
2016-08-20 22:26:12 -0500

    practica 19

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica19/javaconfig/bean/QuadraticEquation.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica19/javaconfig/bean/QuadraticEquation.java
@@ -1,9 +1,15 @@
 package org.certificatic.spring.core.practica19.javaconfig.bean;
 
+import javax.inject.Inject;
+
 import org.certificatic.spring.core.practica19.javaconfig.bean.api.IQuadraticEquationService;
 import org.certificatic.spring.core.practica19.javaconfig.bean.api.QuadraticEquationResult;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 // Define Bean prototype
+@Component
+@Scope("prototype")
 public class QuadraticEquation {
 
 	public enum QuadraticEquationValue {
@@ -15,7 +21,10 @@ public class QuadraticEquation {
 	private double c;
 
 	// Inyecta Dependencia
-	private IQuadraticEquationService quadraticService;
+	@Inject
+	private IQuadraticEquationService quadraticService; // para probar en master
+														// agregar @Setter
+														// @Getter
 
 	public void setValue(double value, QuadraticEquationValue type) {
 		switch (type) {
@@ -38,4 +47,17 @@ public class QuadraticEquation {
 		return this.quadraticService.solve(this.a, this.b, this.c);
 	}
 
+	/*public static void main(String[] args) {
+		QuadraticEquation qe = new QuadraticEquation();
+	
+		qe.setQuadraticService(new QuadraticEquationServiceImpl());
+	
+		qe.setValue(-2, QuadraticEquationValue.Ax2);
+		qe.setValue(5, QuadraticEquationValue.Bx);
+		qe.setValue(-10, QuadraticEquationValue.C);
+	
+		System.out.println("ecuacion: " + qe.getQuadraticService()
+				.quadraticEquationToString(-2, 5, -10));
+		System.out.println("resultado: " + qe.solve());
+	}*/
 }
--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica19/javaconfig/bean/api/QuadraticEquationResult.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica19/javaconfig/bean/api/QuadraticEquationResult.java
@@ -20,5 +20,4 @@ public class QuadraticEquationResult {
 
 		return sb.toString();
 	}
-
 }
