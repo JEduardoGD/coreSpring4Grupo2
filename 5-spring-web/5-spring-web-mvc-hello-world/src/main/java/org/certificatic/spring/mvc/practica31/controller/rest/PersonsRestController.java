e22eb2a5db52709b6d23436c6951d83f31ad3f91
Ivan Van Halen <Ivan Van Halen>
2016-10-15 15:33:57 -0500

    practica 31 - clase 11

--- 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica31/controller/rest/PersonsRestController.java
+++ 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica31/controller/rest/PersonsRestController.java
@@ -10,15 +10,29 @@ import org.certificatic.spring.mvc.practica31.controller.advice.RestResponseErro
 import org.certificatic.spring.validation.practica30.parte1.domain.Person;
 import org.certificatic.spring.validation.practica30.parte1.domain.Persons;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+// /persons
+// /rest/persons
+
+@Slf4j
 // Anotar RestController
+@RestController
 // Anotar request mapping "/rest/persons"
+@RequestMapping("/rest/persons")
 public class PersonsRestController {
 
-	private List<Person> persons = Collections.synchronizedList(new ArrayList<Person>());
+	private List<Person> persons = Collections
+			.synchronizedList(new ArrayList<Person>());
 
 	@PostConstruct
 	private void init() {
@@ -32,24 +46,39 @@ public class PersonsRestController {
 	}
 
 	// Anotar request mapping "/", "", con metodo GET y produciendo json y xml
+	@RequestMapping(value = { "/",
+			"" }, method = RequestMethod.GET, produces = {
+					MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE })
 	public Persons getAllPersons() {
 		return new Persons(persons);
 	}
 
 	// Anotar request mapping "/{id}", con metodo GET y produciendo json y xml
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	// Anotar response status ok
+	@ResponseStatus(code = HttpStatus.OK)
 	public Person getPerson(@PathVariable Integer id) {
 		return persons.get(id - 1);
 	}
 
 	// Anotar request mapping "/", "", con metodo POST y produciendo json y xml
+	@RequestMapping(value = { "/",
+			"" }, method = RequestMethod.POST, produces = {
+					MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE })
 	// Anotar response status NO CONTENT
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void createPerson(@RequestBody Person person) {
+		log.info("create person: {}", person);
 		persons.add(person);
 	}
 
 	// Anotar request mapping "/getException", con metodo GET y produciendo json
 	// y xml. Analizar implementación
+	@RequestMapping(value = "/getException", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<RestResponseError> getException() {
 		try {
 			throw new IllegalArgumentException("Argumentos Inválidos");
@@ -57,10 +86,12 @@ public class PersonsRestController {
 		} catch (IllegalArgumentException ex) {
 			String errorMessage = "Exception: " + ex.getMessage();
 
-			RestResponseError rre = new RestResponseError(HttpStatus.BAD_REQUEST, errorMessage,
+			RestResponseError rre = new RestResponseError(
+					HttpStatus.BAD_REQUEST, errorMessage,
 					ex.getClass().getSimpleName());
 
-			return new ResponseEntity<RestResponseError>(rre, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<RestResponseError>(rre,
+					HttpStatus.BAD_REQUEST);
 		}
 	}
 
