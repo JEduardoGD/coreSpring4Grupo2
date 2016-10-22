3457b8a0415288877eb8cb401e978f1d0d71a5a7
Eduardo Gútiérrez <eduardo_gd@hotmail.com>
2016-10-22 09:28:59 -0500

    cambios del dia

--- 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica31/controller/PersonsController.java
+++ 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica31/controller/PersonsController.java
@@ -5,17 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.print.attribute.standard.Media;
 
 import org.certificatic.spring.validation.practica30.parte1.domain.Person;
 import org.certificatic.spring.validation.practica30.parte1.domain.Persons;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 // Anotar RestController
 // Anotar request mapping "/persons"
+@RequestMapping("/person")
 public class PersonsController {
 
 	private List<Person> persons = Collections.synchronizedList(new ArrayList<Person>());
@@ -32,6 +37,8 @@ public class PersonsController {
 	}
 
 	// Anotar request mapping "/", "", con metodo GET y produciendo json y xml
+	@RequestMapping(value = { "/", "" }, method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
 	public Persons getAllPersons() {
 		return new Persons(persons);
 	}
